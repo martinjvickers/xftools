3436d08f1ec240acecbc4a0357d9459b43c86aee
Martin Vickers <rudigarude@googlemail.com>
2017-01-26 15:30:26 +0000

    Fixed bug which caused the first row after a merged section has been printed to be missed.

--- gff_feature_merge/gff_feature_merge.cpp
+++ gff_feature_merge/gff_feature_merge.cpp
@@ -1,11 +1,12 @@
 #include "common.h"
 #include <unordered_map>
+#include <set>
 
 struct ModifyStringOptions
 {
         CharString inputFileName;
 	CharString outputFileName;
-	bool exclude;
+	int size;
 };
 
 seqan::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & options, int argc, char const ** argv)
@@ -15,11 +16,13 @@ seqan::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & option
 	setRequired(parser, "input-file");
 	addOption(parser, seqan::ArgParseOption("o", "output-file", "Path to the output file", seqan::ArgParseArgument::OUTPUT_FILE, "OUT"));
 	setRequired(parser, "output-file");
+	addOption(parser, seqan::ArgParseOption("s", "size", "Merge Size", seqan::ArgParseArgument::INTEGER, "INT"));
+	setDefaultValue(parser, "size", "4");
 
 	setShortDescription(parser, "GFF Feature Merge");
 	setVersion(parser, "0.0.1");
 	setDate(parser, "Jan 2017");
-	addUsageLine(parser, "-i input.gff -o output.gff [\\fIOPTIONS\\fP] ");
+	addUsageLine(parser, "-i input.gff-o output.gff [\\fIOPTIONS\\fP] ");
 
 	addDescription(parser, "Given a GFF of features, this program will merge features into single features if they lay within a defined (user supplied) range.");
 	seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
@@ -31,12 +34,13 @@ seqan::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & option
 
 	getOptionValue(options.inputFileName, parser, "input-file");
 	getOptionValue(options.outputFileName, parser, "output-file");
+	getOptionValue(options.size, parser, "size");
 
 	return seqan::ArgumentParser::PARSE_OK;
 }
 
 /*
-Aim: Given an annotation file and an input w1 file, this will add all of the w1 records that are within that annotation file.
+Aim:
 Current progress: It compiles!
 */
 int main(int argc, char const ** argv)
@@ -54,10 +58,48 @@ int main(int argc, char const ** argv)
                 return 1;
         }
 
+	//output GFF to cout
+	GffFileOut gffOut(std::cout, Gff());
+
+	GffRecord last_record;
+	GffRecord merging;
+	vector<GffRecord> merged;
+	int count = 0;
+	bool meh = true;
 	//put all of our annotations into a vector and store the pairs in an interval map
 	while (!atEnd(gffIn))
         {
 		readRecord(record, gffIn);
+		if(count==0)
+		{
+			merging = record;
+			meh = false;
+		} else if(meh == true) {
+			merging = record;
+			meh = false;
+		} else {
+			if( (record.ref == merging.ref) && ((record.beginPos - merging.endPos) < options.size) )
+	               	{
+		//		cout << "Extending " << merging.ref << " " << merging.beginPos << " " << merging.endPos << endl;
+				merging.endPos = record.endPos;
+				meh = false;
+			} else {
+				//this is where we have finished the merging
+				//cout << merging << endl;
+				clear(merging.tagNames);
+				clear(merging.tagValues);
+				merging.score = GffRecord::INVALID_SCORE();
+				appendValue(merging.tagNames, "windowSize");
+				appendValue(merging.tagValues, to_string(merging.endPos-merging.beginPos));
+				//merging.score = 0;
+				writeRecord(gffOut, merging);
+				clear(merging);
+				merging = record;
+		//		meh = true;
+			}
+		}
+		//move along here, nothing to see
+		count++;
 	}
 
 	return 0;
