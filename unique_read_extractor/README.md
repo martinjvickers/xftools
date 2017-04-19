036c0e7ae3dde6b6cd29e5de70e648623992b19a
Martin Vickers <rudigarude@googlemail.com>
2017-04-19 15:01:58 +0100

    Inital commit of unique_read_extractor

---
+++ unique_read_extractor/README.md
@@ -0,0 +1,38 @@
+# methylation_tools
+
+## unique_read_extractor
+
+### Compiling
+
+Getting this to compile on work machine;
+
+```
+~/cmake-3.6.0-rc2-Linux-x86_64/bin/cmake ../unique_read_extractor -DCMAKE_MODULE_PATH=../../seqan/util/cmake -DSEQAN_INCLUDE_PATH=../../seqan/include -DCMAKE_CXX_FLAGS=-std=c++14 -DCMAKE_BUILD_TYPE=Release
+```
+
+## Getting started
+
+The program is run like this;
+
+```
+unique_read_extractor -i accepted_hits.sam -o unique.sam
+```
+
+You need to convert your BAM files into SAM files before hand;
+
+```
+samtools view -h -b -o accepted_hits.sam accepted_hits.bam
+```
+
+and then to convert the unique SAM file to a sorted BAM file for IGV;
+
+```
+samtools view -bS unique.sam | samtools sort - -o unique_sorted.bam
+samtools index unique_sorted.bam
+```
+
+### MY ASSUMPTIONS SO FAR
+
+* Since it's the tophat accepted hits file there are no unmapped reads
+* Data is not sorted by read name hence the need to store names in RAM
+* A unique read is defined as a read name that has only ever mapped once
---
+++ unique_read_extractor/unique_read_extractor.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <seqan/sequence.h>  // CharString, ...
+#include <seqan/stream.h>    // to stream a CharString into cout
+#include <seqan/file.h>
+#include <seqan/arg_parse.h>
+#include <seqan/bam_io.h>
+#include <math.h>       /* sqrt */
+#include <seqan/store.h> /* FragmentStore */
+#include <queue>
+#include <vector>
+#include <ctime>
+#include "boost/multi_array.hpp"
+#include <cassert>
+#include <boost/unordered_map.hpp>
+#include <string>
+#include <thread>
+#include <mutex>
+#include <stdlib.h>
+#include <stdio.h>
+#include <map>
+#include <iomanip>
+using namespace seqan;
+using namespace std;
+
+struct ModifyStringOptions
+{
+        CharString inputFileName;
+	CharString outputFileName;
+};
+
+seqan::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & options, int argc, char const ** argv)
+{
+	seqan::ArgumentParser parser("fast_extract");
+	addOption(parser, seqan::ArgParseOption("i", "input-file", "Path to the input file", seqan::ArgParseArgument::INPUT_FILE, "IN"));
+	setRequired(parser, "input-file");
+	addOption(parser, seqan::ArgParseOption("o", "output-file", "Path to the output file", seqan::ArgParseArgument::OUTPUT_FILE, "OUT"));
+	setRequired(parser, "output-file");
+
+	setShortDescription(parser, "unique_read_extractor");
+	setVersion(parser, "0.0.1");
+	setDate(parser, "Jan 2017");
+	addUsageLine(parser, "-i input.bam -o output.bam [\\fIOPTIONS\\fP] ");
+
+	addDescription(parser, "Exclude any mapped read that occurs more than one.");
+	seqan::ArgumentParser::ParseResult res = seqan::parse(parser, argc, argv);
+
+	// If parsing was not successful then exit with code 1 if there were errors.
+	// Otherwise, exit with code 0 (e.g. help was printed).
+	if (res != seqan::ArgumentParser::PARSE_OK)
+		return res;
+
+	getOptionValue(options.inputFileName, parser, "input-file");
+	getOptionValue(options.outputFileName, parser, "output-file");
+
+	return seqan::ArgumentParser::PARSE_OK;
+}
+
+/*
+Aim: Ronseal, do what it says on the tin
+
+Current progress: It compiles!
+
+*/
+int main(int argc, char const ** argv)
+{
+	//parse our options
+	ModifyStringOptions options;
+	seqan::ArgumentParser::ParseResult res = parseCommandLine(options, argc, argv);
+
+	//open the fasta/fastq file	
+	BamFileIn bamFileIn;
+	if (!open(bamFileIn, toCString(options.inputFileName)))
+	{
+		cerr << "ERROR: Could not open file." << endl;
+		return 1;
+	}
+
+	map<string,int> counter;
+
+	//output stream
+	BamFileOut bamFileOut(context(bamFileIn), toCString(options.outputFileName));
+	
+	try
+	{
+		//Read the header
+		BamHeader header;
+		readHeader(header, bamFileIn);
+		BamAlignmentRecord record;
+
+        	while (!atEnd(bamFileIn))
+        	{
+			readRecord(record, bamFileIn);
+			counter[toCString(record.qName)]++;
+		}
+
+		//at the begining and reread the header to get back to the first read position
+		setPosition(bamFileIn,0);
+		BamHeader outputheader;
+		readHeader(outputheader, bamFileIn);
+		writeHeader(bamFileOut, outputheader);
+
+		//now cycl through
+		while (!atEnd(bamFileIn))
+                {
+			readRecord(record, bamFileIn);
+			if(counter[toCString(record.qName)] < 2)
+			{
+				writeRecord(bamFileOut, record);	
+			}
+		}
+	}
+	catch (Exception const & e)
+	{
+		std::cout << "ERROR: " << e.what() << std::endl;
+		return 1;
+	}
+
+	
+
+	return 0;
+}
