e3fd78deea56d9a8b457390362cd38b2dd1e40a9
Martin Vickers <rudigarude@googlemail.com>
2017-05-22 10:56:39 +0100

    Initial commit of SeqAn 2.2.0 to XFTOOLS

---
+++ seqan/tests/alfsc/CMakeFiles/test_alfsc.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/users/JIC_c1/mvickers/development/seqan/tests/alfsc/test_alfsc.cpp" "/usr/users/JIC_c1/mvickers/development/seqan/tests/alfsc/CMakeFiles/test_alfsc.dir/test_alfsc.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SEQAN_HAS_EXECINFO=1"
+  "_FILE_OFFSET_BITS=64"
+  "_LARGEFILE_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/users/JIC_c1/mvickers/development/seqan/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
