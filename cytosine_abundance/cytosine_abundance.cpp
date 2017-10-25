004eb676baf21e1891e6e9fdcf470ead2a064714
Martin Vickers <rudigarude@googlemail.com>
2017-10-25 16:25:18 +0100

    Seems to be working except for at the start and end of a contig

--- cytosine_abundance/cytosine_abundance.cpp
+++ cytosine_abundance/cytosine_abundance.cpp
@@ -53,6 +53,109 @@ seqan::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & option
         return seqan::ArgumentParser::PARSE_OK;
 }
 
+unsigned int count_Cs (Dna5String &seq)
+{
+	unsigned int counter = 0;
+
+	for(unsigned int i = 0; i < length(seq); i++)
+		if(seq[i] == 'C')
+			counter++;
+	return counter;
+}
+
+void calculate_contexts(Dna5String &seq, map<string,int> &context_map)
+{
+	for(unsigned i = 2; i < length(seq)-2; i++)
+	{
+		Infix<String<Dna5> >::Type inf = infix(seq, i, i+3);
+		CharString trin = inf;
+		if(inf[0] == 'C')
+		{
+			if(inf[1] == 'G')
+			{
+				context_map["CG"]++;		//CG
+				context_map[toCString(trin)]++;
+			}
+			else
+			{
+				if(inf[i+2] == 'G')
+				{
+					context_map["CHG"]++;	//CHG
+					context_map[toCString(trin)]++;
+				}
+				else
+				{
+					context_map["CHH"]++; 	//CHH
+					context_map[toCString(trin)]++;
+				}
+			}
+		}
+	}
+}
+
+void print_map(map<string,int> &context_map)
+{
+	for (auto i : context_map)
+		cout << i.first << "=" << i.second << endl;
+}
+
+void new_context_map(map<string,int> &context_map)
+{
+	context_map.insert( make_pair("CG", 0));
+	context_map.insert( make_pair("CGA", 0));
+	context_map.insert( make_pair("CGC", 0));
+	context_map.insert( make_pair("CGG", 0));
+	context_map.insert( make_pair("CGT", 0));
+	context_map.insert( make_pair("CGN", 0));
+	context_map.insert( make_pair("CHG", 0));
+	context_map.insert( make_pair("CAG", 0));
+	context_map.insert( make_pair("CCG", 0));
+	context_map.insert( make_pair("CTG", 0));
+	context_map.insert( make_pair("CNG", 0));
+	context_map.insert( make_pair("CHH", 0));
+	context_map.insert( make_pair("CAA", 0));
+	context_map.insert( make_pair("CAC", 0));
+	context_map.insert( make_pair("CAT", 0));
+	context_map.insert( make_pair("CCA", 0));
+	context_map.insert( make_pair("CCC", 0));
+	context_map.insert( make_pair("CCT", 0));
+	context_map.insert( make_pair("CTA", 0));
+	context_map.insert( make_pair("CTC", 0));
+	context_map.insert( make_pair("CTT", 0));
+	context_map.insert( make_pair("CNN", 0));
+}
+
+void write_record(GffFileOut &gffOutFile, FaiIndex &faiIndex, unsigned int &contig, unsigned int &beginPos, int endPos, unsigned int &C_count, map<string,int> &context_map, ModifyStringOptions &options)
+{
+	GffRecord record;
+	record.ref = sequenceName(faiIndex, contig);
+	record.source = "xftools";
+	record.type = options.label;
+	record.beginPos = beginPos;
+	record.endPos = endPos;
+	record.score = C_count;
+	record.strand = '.';
+	record.phase = '.';
+	StringSet<CharString> tagNames;
+	StringSet<CharString> tagValues;
+	for (auto i : context_map)
+	{
+        	appendValue(record.tagNames, toCString(i.first));
+		//CharString m = i.second;
+		appendValue(record.tagValues, to_string(i.second));
+		//appendValue(record.tagValues, toCString(i.second));
+	}
+	writeRecord(gffOutFile, record);
+}
+
+/*
+	Method.
+          Create index of reference
+          Loop through the reference in windows
+            count c's + contexts
+            reverse compliment and repeat
+            print to GFF
+*/
 int main(int argc, char const ** argv)
 {
 	ModifyStringOptions options;
@@ -80,6 +183,51 @@ int main(int argc, char const ** argv)
     	} else {
 		cout << "Index created of reference file. " << endl;
 	}
+
+	// I need to know the number of contigs
+	Dna5String reference_seq;
+
+	// for each contig
+	for(unsigned int contig = 0; contig < numSeqs(faiIndex); contig++)
+	{
+		unsigned int beginPos = 0;
+		while (beginPos < sequenceLength(faiIndex, contig))
+		{
+			map<string,int> context_map;
+			new_context_map(context_map);
+
+			readRegion(reference_seq, faiIndex, contig, beginPos, beginPos + options.window_size);
+
+			// count the c's
+			unsigned int total_C = count_Cs(reference_seq);
+			reverseComplement(reference_seq);
+			total_C = total_C + count_Cs(reference_seq);
+			//cout << contig << "\t" << beginPos << "\t" << beginPos + options.window_size << "\t" << total_C << endl;
+
+			// contexts
+			if(beginPos > 0 && (beginPos+options.window_size+2 < sequenceLength(faiIndex, contig)) )
+			{
+				// get what we want with +2 on either side
+				readRegion(reference_seq, faiIndex, contig, beginPos-2, beginPos + options.window_size+2);
+				calculate_contexts(reference_seq, context_map);
+				reverseComplement(reference_seq);
+				calculate_contexts(reference_seq, context_map);
+			//	print_map(context_map);
+			}
+			else 
+			{
+				// i will work out exceptions in a bit
+				//cout << length(reference_seq) << endl;
+			}
+
+			write_record(gffOutFile, faiIndex, contig, beginPos, beginPos + options.window_size, total_C, context_map, options);
+
+			beginPos = beginPos + options.window_size;
+		}
+		// Do the last one too. Will work that out later but it's just this
+		// readRegion(reference_seq, faiIndex, contig, beginPos, sequenceLength(faiIndex, contig));
+	}
+
 
 	// method.
 	// create index of reference
