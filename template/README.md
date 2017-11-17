2c592b9d8e24768ce3c5604a08991c409bdee468
Martin Vickers <rudigarude@googlemail.com>
2017-11-17 14:14:27 +0000

    Initial commit for a quickstart template

---
+++ template/README.md
@@ -0,0 +1,26 @@
+# XFTOOLS
+
+## TOOLNAME
+
+Description of TOOLNAME
+
+### Compiling
+
+Getting this to compile on work machine;
+
+```
+~/cmake-3.6.0-rc2-Linux-x86_64/bin/cmake ../TOOLNAME \
+   -DCMAKE_MODULE_PATH=../../seqan/util/cmake \
+   -DSEQAN_INCLUDE_PATH=../../seqan/include \
+   -DCMAKE_CXX_FLAGS=-std=c++14 \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DCMAKE_CXX_ARGS="-DSEQAN_DISABLE_VERSION_CHECK=YES" 
+```
+
+### Running
+
+### TODEL: Template instructions
+
+TODEL: You should be able to `sed` for TOOLNAME within this doc as well as
+TODEL: CMakeList.txt and TOOLNAME.cpp in order to rename. Also grep out
+TODEL: the TODEL lines within this doc.
---
+++ template/TOOLNAME.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <seqan/arg_parse.h>
+
+using namespace seqan;
+using namespace std;
+
+struct ModifyStringOptions {
+   CharString inputFileName;
+   CharString inputAnnotationFileName;
+   CharString outputFileName;
+   bool exclude;
+   bool lazyRef;
+};
+
+seqan::ArgumentParser::ParseResult parseCommandLine(
+	ModifyStringOptions & options, 
+	int argc, 
+	char const ** argv) {
+
+   ArgumentParser parser("Overlap");
+   addOption(parser, ArgParseOption("i", "input-file", 
+                                    "Path to the input file", 
+                                    ArgParseArgument::INPUT_FILE,
+                                    "IN"));
+   setRequired(parser, "input-file");
+   addOption(parser, ArgParseOption("a", "input-annotation-file", 
+                                    "Path to the input filter file", 
+                                    ArgParseArgument::INPUT_FILE, 
+                                    "IN"));
+   setRequired(parser, "input-annotation-file");
+   addOption(parser, ArgParseOption("o", "output-file", 
+                                    "Path to the output file", 
+                                    ArgParseArgument::OUTPUT_FILE,
+                                    "OUT"));
+   setRequired(parser, "output-file");
+   addOption(parser, ArgParseOption("l", "lazy-ref", 
+       "Internally it will capitalise both the input and annoation reference \
+        names so that chr1, Chr1 and CHR1 will all match. The output GFF will \
+        be of the same format as the annoation file."));
+   setShortDescription(parser, "Overlap");
+   setVersion(parser, "0.0.1");
+   setDate(parser, "November 2017");
+   addUsageLine(parser, "-i input.gff -a annotation.gff -o output.gff \
+                         [\\fIOPTIONS\\fP] ");
+   addDescription(parser, "Given an annotation/feature file and an input w1 \
+                           file, give counts for each feature");
+   ArgumentParser::ParseResult res = parse(parser, argc, argv);
+
+   if (res != ArgumentParser::PARSE_OK)
+      return res;
+
+   getOptionValue(options.inputFileName, parser, "input-file");
+   getOptionValue(options.inputAnnotationFileName, 
+                  parser, "input-annotation-file");
+   getOptionValue(options.outputFileName, parser, "output-file");
+   options.lazyRef = isSet(parser, "lazy-ref");
+
+   return ArgumentParser::PARSE_OK;
+}
+
+// A basic template to get up and running quickly
+int main(int argc, char const ** argv)
+{
+   //parse our options
+   ModifyStringOptions options;
+   ArgumentParser::ParseResult res = parseCommandLine(options,argc, argv);
+   if (res != ArgumentParser::PARSE_OK)
+      return res == ArgumentParser::PARSE_ERROR;
+
+   return 0;
+}
