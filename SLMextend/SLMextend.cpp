8c1fdba95d5a9751f4a38a64590ed495e4aa4c27
Martin Vickers <rudigarude@googlemail.com>
2019-02-15 15:19:15 +0000

    Read files

--- SLMextend/SLMextend.cpp
+++ SLMextend/SLMextend.cpp
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <seqan/arg_parse.h>
+#include <seqan/gff_io.h>
+#include <seqan/misc/interval_tree.h>
 
 using namespace seqan;
 using namespace std;
 
 struct ModifyStringOptions {
-   CharString inputFileName;
-   CharString inputAnnotationFileName;
-   CharString outputFileName;
-   bool exclude;
-   bool lazyRef;
+   CharString inputSLMFileName;
+   CharString inputSpermFileName;
+   CharString inputS1FileName;
+   CharString inputS2FileName;
+   CharString inputS3FileName;
 };
 
 seqan::ArgumentParser::ParseResult parseCommandLine(
@@ -20,25 +22,36 @@ seqan::ArgumentParser::ParseResult parseCommandLine(
 	char const ** argv) {
 
    ArgumentParser parser("SLMextend");
-   addOption(parser, ArgParseOption("i", "input-file", 
+   addOption(parser, ArgParseOption("slm", "slm-file", 
                                     "Path to the input file", 
                                     ArgParseArgument::INPUT_FILE,
                                     "IN"));
-   setRequired(parser, "input-file");
-   addOption(parser, ArgParseOption("a", "input-annotation-file", 
+   setRequired(parser, "slm-file");
+
+   addOption(parser, ArgParseOption("spm", "sperm-file", 
                                     "Path to the input filter file", 
                                     ArgParseArgument::INPUT_FILE, 
                                     "IN"));
-   setRequired(parser, "input-annotation-file");
-   addOption(parser, ArgParseOption("o", "output-file", 
-                                    "Path to the output file", 
-                                    ArgParseArgument::OUTPUT_FILE,
-                                    "OUT"));
-   setRequired(parser, "output-file");
-   addOption(parser, ArgParseOption("l", "lazy-ref", 
-       "Internally it will capitalise both the input and annoation reference \
-        names so that chr1, Chr1 and CHR1 will all match. The output GFF will \
-        be of the same format as the annoation file."));
+   setRequired(parser, "sperm-file");
+
+   addOption(parser, ArgParseOption("s1", "soma1-file",                
+                                    "Path to the input filter file",
+                                    ArgParseArgument::INPUT_FILE,
+                                    "IN"));
+   setRequired(parser, "soma1-file");
+
+   addOption(parser, ArgParseOption("s2", "soma2-file",     
+                                    "Path to the input filter file",
+                                    ArgParseArgument::INPUT_FILE,
+                                    "IN"));
+   setRequired(parser, "soma2-file");
+
+   addOption(parser, ArgParseOption("s3", "soma3-file",
+                                    "Path to the input filter file",
+                                    ArgParseArgument::INPUT_FILE,
+                                    "IN"));
+   setRequired(parser, "soma3-file");
+
    setShortDescription(parser, "SLMextend");
    setVersion(parser, "0.0.1");
    setDate(parser, "February 2019");
@@ -52,16 +65,57 @@ seqan::ArgumentParser::ParseResult parseCommandLine(
    if (res != ArgumentParser::PARSE_OK)
       return res;
 
-   getOptionValue(options.inputFileName, parser, "input-file");
-   getOptionValue(options.inputAnnotationFileName, 
-                  parser, "input-annotation-file");
-   getOptionValue(options.outputFileName, parser, "output-file");
-   options.lazyRef = isSet(parser, "lazy-ref");
+   getOptionValue(options.inputSLMFileName, parser, "slm-file");
+   getOptionValue(options.inputSpermFileName, parser, "sperm-file");
+   getOptionValue(options.inputS1FileName, parser, "soma1-file");
+   getOptionValue(options.inputS2FileName, parser, "soma2-file");
+   getOptionValue(options.inputS3FileName, parser, "soma3-file");
 
    return ArgumentParser::PARSE_OK;
 }
 
-// A basic template to get up and running quickly
+typedef IntervalAndCargo<int, GffRecord> TInterval;
+
+int insertIntervals(map<CharString, String<TInterval>> &intervals, 
+                    GffFileIn &gffInFile)
+{
+   GffRecord record;
+
+   try
+   {
+      while(!atEnd(gffInFile))
+      {
+         readRecord(record, gffInFile);
+         appendValue(intervals[record.ref], 
+                     TInterval(record.beginPos, record.endPos, record));
+      }
+   }
+   catch (Exception const & e)
+   {
+      cerr << "ERROR: " << e.what() << std::endl;
+      return 1;
+   }
+
+   close(gffInFile);
+   return 0;
+}
+
+
+/*
+Brief from Jimmy
+
+Take SLM.
+Take the adjacent 100bp from either side of SLM
+
+Calculate the % methylation for these 100bp regions for the sperm and three 
+somatic tissues using c's and t's
+
+Calculate the average % methylation for soma using the three individual %.
+
+If the % methylation difference between sperm and the average soma is 
+greater than x, then add the region onto the SLM. Repeat until the previous 
+statement is untrue.
+*/
 int main(int argc, char const ** argv)
 {
    //parse our options
@@ -69,6 +123,67 @@ int main(int argc, char const ** argv)
    ArgumentParser::ParseResult res = parseCommandLine(options,argc, argv);
    if (res != ArgumentParser::PARSE_OK)
       return res == ArgumentParser::PARSE_ERROR;
+
+   // read in the SLM file
+   GffFileIn gffSLMInFile;
+   if(!open(gffSLMInFile, toCString(options.inputSLMFileName)))
+   {
+      cerr << "ERROR: Could not open ";
+      cerr << options.inputSLMFileName;
+      cerr << " for reading.\n";
+      return 1;
+   }
+
+   // load the sperm and soma files
+   GffFileIn gffSpermInFile;
+   if(!open(gffSpermInFile, toCString(options.inputSpermFileName)))
+   {
+      cerr << "ERROR: Could not open ";
+      cerr << options.inputSpermFileName;
+      cerr << " for reading.\n";
+      return 1;
+   }
+
+   GffFileIn gffS1InFile;
+   if(!open(gffS1InFile, toCString(options.inputS1FileName)))
+   {
+      cerr << "ERROR: Could not open ";
+      cerr << options.inputS1FileName;
+      cerr << " for reading.\n";
+      return 1;
+   }
+
+   GffFileIn gffS2InFile;
+   if(!open(gffS2InFile, toCString(options.inputS2FileName)))
+   {
+      cerr << "ERROR: Could not open ";
+      cerr << options.inputS2FileName;
+      cerr << " for reading.\n";
+      return 1;
+   }
+
+   GffFileIn gffS3InFile;
+   if(!open(gffS3InFile, toCString(options.inputS3FileName)))
+   {
+      cerr << "ERROR: Could not open ";
+      cerr << options.inputS3FileName;
+      cerr << " for reading.\n";
+      return 1;
+   }
+      
+   // for each SLM
+
+      // do
+
+         // get <window and >window soma average methylation
+         // get <window and >window sperm average methylation
+
+         // if abs(sperm-soma) > x, extend SLM
+
+      // if SLM extended, loop
+
+
+
 
    return 0;
 }
